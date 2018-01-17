effaffb1c78ba2e8853e03a617c2380b09360e33
Harry Markley <tremarkley@gmail.com>
2018-01-17 12:18:02 -0800

    Setup base for stack using prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,19 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.storage = {};
+  obj.count = 0;
+  obj.currentIndex = 0;
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function() {},
+  pop: function() {},
+  size: function() {
+    return this.count;
+  }
+};
 
 
