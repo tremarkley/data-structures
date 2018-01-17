800f3b2d4cee4f2ac64c97a424a9b99de065c4e1
Harry Markley <tremarkley@gmail.com>
2018-01-17 14:26:59 -0800

    Fix styling for prototypal queue.js

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,6 +1,4 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   var obj = Object.create(queueMethods);
   obj.storage = {};
   obj.currentIndex = 0;
@@ -10,23 +8,23 @@ var Queue = function() {
 };
 
 var queueMethods = {
-    enqueue: function(value) {
-      this.count++;
-      this.storage[this.lastIndex] = value;
-      this.lastIndex++;
-    },
-    dequeue: function() {
-      if (this.count > 0) {
-        this.count--;
-        let returnVal = this.storage[this.currentIndex];
-        delete this.storage[this.currentIndex];
-        this.currentIndex++;
-        return returnVal;
-      }
-    },
-    size: function() {
-      return this.count;
+  enqueue: function(value) {
+    this.count++;
+    this.storage[this.lastIndex] = value;
+    this.lastIndex++;
+  },
+  dequeue: function() {
+    if (this.count > 0) {
+      this.count--;
+      let returnVal = this.storage[this.currentIndex];
+      delete this.storage[this.currentIndex];
+      this.currentIndex++;
+      return returnVal;
     }
+  },
+  size: function() {
+    return this.count;
+  }
 }
 
 
