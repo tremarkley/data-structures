0bfb244df421df529b971d891087eebae243d013
Hack Reactor Student <student@hackreactor.com>
2018-01-17 15:22:25 -0800

    Passes test for removeHead

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,10 +4,19 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-    this.tail = Node(value);
+    let newNode = Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
   };
 
   list.removeHead = function() {
+    let previousHead = this.head;
+    this.head = this.head.next;
+    return previousHead.value;
   };
 
   list.contains = function(target) {
