43fe8e4bc84ff160188f623aa317924b202dd4a0
Harry Markley <tremarkley@gmail.com>
2018-01-17 11:54:48 -0800

    Implement queue with functional-shared pattern and all tests passing

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -4,18 +4,27 @@ var Queue = function() {
   var obj = {
    storage: {},
    count: 0,
-   currentIndex: {},
-   lastIndex: {}
+   currentIndex: 0,
+   lastIndex: 0
   };
   _.extend(obj, queueMethods);
   return obj;
 };
 
 var queueMethods = {
-  enqueue: function() {
+  enqueue: function(value) {
     this.count++;
+    this.storage[this.lastIndex] = value;
+    this.lastIndex++;
   },
-  dequeue: function() {},
+  dequeue: function() {
+    if (this.count > 0) {
+      this.count--;
+      let returnVal = this.storage[this.currentIndex];
+      this.currentIndex++;
+      return returnVal;
+    }
+  },
   size: function() {
     return this.count;
   }
