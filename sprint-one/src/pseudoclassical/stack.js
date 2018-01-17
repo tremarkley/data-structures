f501bc5c136d0e9130b390f3a6b41bc6fe3d5f79
Harry Markley <tremarkley@gmail.com>
2018-01-17 13:17:45 -0800

    Implement stack with all tests passing except for constructor test

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -6,8 +6,18 @@ var Stack = function() {
 };
 
 Stack.prototype = {
-  push: function() {},
-  pop: function() {},
+  push: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  pop: function() {
+    if (this.count > 0) {
+      this.count--;
+      let returnVal = this.storage[this.count];
+      delete this.storage[this.count];
+      return returnVal;
+    }
+  },
   size: function() {
     return this.count;
   }
