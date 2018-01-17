40640158997e2a410965e7fe40898cb6afa7e59c
Harry Markley <tremarkley@gmail.com>
2018-01-17 12:12:16 -0800

    Setup base for queue using protoypal pattern, returning size of 0 when count is 0

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,20 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.storage = {};
+  obj.currentIndex = 0;
+  obj.count = 0;
+  obj.lastIndex = 0;
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+    enqueue: function() {},
+    dequeue: function() {},
+    size: function() {
+      return this.count;
+    }
+}
 
 
