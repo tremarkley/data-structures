2484939e7f961377acfbf067915eaa012d01f600
Harry Markley <tremarkley@gmail.com>
2018-01-17 12:30:36 -0800

    Setup base for pseudoclassical queue but constructor test is failing

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
+  this.currentIndex = 0;
+  this.lastIndex = 0;
 };
 
+Queue.prototype = {
+  enqueue: function() {
+    this.count++;
+  },
+  dequeue: function() {
+    if (this.count > 0) {
+      this.count--;
+    }
+  },
+  size: function() {
+    return this.count;
+  }
+}
 
