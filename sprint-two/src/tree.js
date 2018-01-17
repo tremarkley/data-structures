7f5da272b8bec1487fe2911121403376640c95dd
Hack Reactor Student <student@hackreactor.com>
2018-01-17 15:49:51 -0800

    Implement tree with all tests passing

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -10,11 +10,24 @@ var Tree = function(value) {
 };
 
 var treeMethods = {};
-
+//time complexity O(1);
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
-
+//time complexity O(n);
 treeMethods.contains = function(target) {
+  let returnValue = false;
+  
+  var innerFunction = function(node) {
+    if (node.value === target) {
+      returnValue = true;
+    } 
+    for (let i = 0; i < node.children.length; i++) {
+      innerFunction(node.children[i]);
+    }
+  };
+  innerFunction(this);
+  return returnValue;
 };
 
 
