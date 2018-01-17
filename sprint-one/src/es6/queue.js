b2f5331d1235666088b2040803639db0cd55a7e4
Harry Markley <tremarkley@gmail.com>
2018-01-17 14:37:27 -0800

    Setup base for es6 pattern on queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,21 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+    this.currentIndex = 0;
+    this.lastIndex = 0;
   }
 
+  dequeue() {
+
+  }
+
+  enqueue(value) {
+
+  }
+
+  size() {
+    return this.count;
+  }
 }
