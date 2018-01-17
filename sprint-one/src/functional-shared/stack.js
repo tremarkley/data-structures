42f844e836bfe08a30576839399a18ac56c9811e
Harry Markley <tremarkley@gmail.com>
2018-01-17 11:58:57 -0800

    Setup base for stack with function-shared pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj= {
+    storage: {},
+    count: 0,
+    currentIndex: 0
+  };
+  _.extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function() {
+    this.count++;
+  },
+  pop: function() {},
+  size: function() {
+    return this.count;
+  }
+};
 
 
