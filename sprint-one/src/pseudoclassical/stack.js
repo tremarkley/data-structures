839cb13243b2dcf4bb94b6451594cfdef1e40833
Harry Markley <tremarkley@gmail.com>
2018-01-17 13:15:37 -0800

    Setup base stack with pseudoclassical pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,15 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
 };
 
+Stack.prototype = {
+  push: function() {},
+  pop: function() {},
+  size: function() {
+    return this.count;
+  }
+}
 
