86d4450dc0a14da4558b4ef1d4230c494eb2959f
Harry Markley <tremarkley@gmail.com>
2018-01-17 11:50:46 -0800

    Setup base for functional-shared pattern on queue with returning correct size when 0 items added and 2 items added

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {
+   storage: {},
+   count: 0,
+   currentIndex: {},
+   lastIndex: {}
+  };
+  _.extend(obj, queueMethods);
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function() {
+    this.count++;
+  },
+  dequeue: function() {},
+  size: function() {
+    return this.count;
+  }
+};
 
 
