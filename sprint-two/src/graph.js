e15591fc1674a62da82b5782b388c7de92f55695
Hack Reactor Student <student@hackreactor.com>
2018-01-17 18:12:46 -0800

    Refactored Graph with GraphNode and can add/remove nodes

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -7,7 +7,7 @@ var Graph = function() {
   //this.value;
 };
 
-var Node = function(value) {
+var GraphNode = function(value) {
   this.value = value;
   this.edges = {};
 };
@@ -15,34 +15,36 @@ var Node = function(value) {
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
   //this.nodes[node] = this.edges; // 1: {3 : null, 2 : null}
-  let newNode = new Node(node);
+  let newNode = new GraphNode(node);
   //this.value = node;
-  this.nodes
+  this.nodes[node] = {};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  let foundNode = false;
-  let visitedNodes = {};
-  var innerFunction = function(currentNode) {
-    let val = currentNode.value;
-    if (visitedNodes[val] === undefined) {
-      visitedNodes[val] = '1';
-      if (val === node) {
-        foundNode = true;
-      } else {
-        for (var key in currentNode.edges) {
-          innerFunction(key);
-        }
-      }
-    }
-  };
-  innerFunction(this);
-  return foundNode;
+  // let foundNode = false;
+  // let visitedNodes = {};
+  // var innerFunction = function(currentNode) {
+  //   let val = currentNode.value;
+  //   if (visitedNodes[val] === undefined) {
+  //     visitedNodes[val] = '1';
+  //     if (val === node) {
+  //       foundNode = true;
+  //     } else {
+  //       for (var key in currentNode.edges) {
+  //         innerFunction(key);
+  //       }
+  //     }
+  //   }
+  // };
+  // innerFunction(this);
+  // return foundNode;
+  return this.nodes[node] !== undefined; 
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.nodes[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
