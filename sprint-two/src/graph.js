ad1f0bb3c768736f957e302e5bef9858425d302c
Hack Reactor Student <student@hackreactor.com>
2018-01-17 20:08:02 -0800

    Full implementation of Graph with all tests passing

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,65 +2,57 @@
 
 // Instantiate a new graph
 var Graph = function() {
-  //this.edges = {};
   this.nodes = {};
-  //this.value;
 };
 
-var GraphNode = function(value) {
-  this.value = value;
-  this.edges = {};
-};
-
+// time complexity: O(1)
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  //this.nodes[node] = this.edges; // 1: {3 : null, 2 : null}
-  let newNode = new GraphNode(node);
-  //this.value = node;
   this.nodes[node] = {};
 };
 
+// time complexity: O(1)
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  // let foundNode = false;
-  // let visitedNodes = {};
-  // var innerFunction = function(currentNode) {
-  //   let val = currentNode.value;
-  //   if (visitedNodes[val] === undefined) {
-  //     visitedNodes[val] = '1';
-  //     if (val === node) {
-  //       foundNode = true;
-  //     } else {
-  //       for (var key in currentNode.edges) {
-  //         innerFunction(key);
-  //       }
-  //     }
-  //   }
-  // };
-  // innerFunction(this);
-  // return foundNode;
   return this.nodes[node] !== undefined; 
 };
 
+// time complexity: O(n)
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (let key in this.nodes[node]) {
+    this.removeEdge(node, key);
+  }  
   delete this.nodes[node];
 };
 
+// time complexity: O(1)
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this.nodes[fromNode][toNode] !== undefined;
 };
 
+// time complexity: O(1)
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.nodes[fromNode][toNode] = {};
+  this.nodes[toNode][fromNode] = {};
 };
 
+// time complexity: O(1)
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.nodes[fromNode][toNode];
+  delete this.nodes[toNode][fromNode];
 };
 
+// time complexity: O(n)
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (let key in this.nodes) {
+    cb(key);
+  }
+
 };
 
 /*
