e4e0d02c034f5bc2cb9765c3250faaffdd3b9d51
Hack Reactor Student <student@hackreactor.com>
2018-01-18 15:55:37 -0800

    binarySearchTree all test passing

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,18 +1,49 @@
 var BinarySearchTree = function(value) {
-  this.left;
-  this.right;
+  this.left = undefined;
+  this.right = undefined;
+  this.value = value;
 };
 
-BinarySearchTree.prototype.insert = function() {
+//O(n)
+BinarySearchTree.prototype.insert = function(value) {
+  let newTree = new BinarySearchTree(value);
+  if (this.value < newTree.value) {
+    if (this.right !== undefined) { 
+      this.right.insert(newTree.value);
+    } else {
+      this.right = newTree;
+    }
+  } else {
+    if (this.left !== undefined) { 
+      this.left.insert(newTree.value);
 
+    } else {
+      this.left = newTree;
+    }
+  }
 };
 
-BinarySearchTree.prototype.contains = function() {
-
+//O(n)
+BinarySearchTree.prototype.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else if (this.value < target && this.right !== undefined) {
+    return this.right.contains(target);
+  } else if (this.value > target && this.left !== undefined) {
+    return this.left.contains(target);
+  }
+  return false;
 };
 
-BinarySearchTree.prototype.depthFirstLog = function() {
-
+//O(n)
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left !== undefined) {
+    this.left.depthFirstLog(cb);
+  } 
+  if (this.right !== undefined) {
+    this.right.depthFirstLog(cb);
+  }
 };
 
 /*
