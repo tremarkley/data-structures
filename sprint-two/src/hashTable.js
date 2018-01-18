dfbd74718f8996f7cb556930f5a6cd11e6ccaedc
Hack Reactor Student <student@hackreactor.com>
2018-01-18 10:39:02 -0800

    implement insert for Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,10 +7,23 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let obj = [k, v]; 
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, []);
+  }
+  this._storage.get(index).push(obj);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let bucketArray = this._storage.get(index); 
+  for (let i = 0; i < bucketArray.length; i++) { 
+    let key = bucketArray[i][0];
+    let value = bucketArray[i][1];
+    if (key === k) {
+      return value;
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
