b5806ae1fea46af8e7dcf7d2a4d49db1a2bd247f
Hack Reactor Student <student@hackreactor.com>
2018-01-19 11:16:48 -0800

    Add tree to advanced content and SpecRunner.html

---
+++ advanced/src/tree.js
@@ -0,0 +1,38 @@
+var Tree = function(value) {
+  var newTree = {};
+  newTree.value = value;
+
+  // your code here
+  newTree.children = [];
+  newTree.parent = null;
+  _.extend(newTree, treeMethods);
+
+  return newTree;
+};
+
+var treeMethods = {};
+//time complexity O(1);
+treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
+};
+//time complexity O(n);
+treeMethods.contains = function(target) {
+  let returnValue = false;
+  
+  var innerFunction = function(node) {
+    if (node.value === target) {
+      returnValue = true;
+    } 
+    for (let i = 0; i < node.children.length; i++) {
+      innerFunction(node.children[i]);
+    }
+  };
+  innerFunction(this);
+  return returnValue;
+};
+
+
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
