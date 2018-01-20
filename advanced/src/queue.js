fac220f30ee47fd35a2b5505971670858890906f
Hack Reactor Student <student@hackreactor.com>
2018-01-20 09:29:43 -0800

    Add queue to SpecRunner

---
+++ advanced/src/queue.js
@@ -0,0 +1,27 @@
+var Queue = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
+  this.currentIndex = 0;
+  this.lastIndex = 0;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.count++;
+  this.storage[this.lastIndex] = value;
+  this.lastIndex++;
+};
+Queue.prototype.dequeue = function() {
+  if (this.count > 0) {
+    this.count--;
+    let returnVal = this.storage[this.currentIndex];
+    delete this.storage[this.currentIndex];
+    this.currentIndex++;
+    return returnVal;
+  }
+};
+Queue.prototype.size = function() {
+  return this.count;
+};
+
